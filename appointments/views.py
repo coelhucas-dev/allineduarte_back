6797671b375bde81fdd1a60b036af54d5b39524a
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-04-14 14:01:26 -0300

    create AppointmentCreateView

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from appointments.models import Appointment
+from appointments.serializers import AppointmentSerializer
+
+
+class AppointmentCreateView(CreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
