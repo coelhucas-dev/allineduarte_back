8224728f6c3b5db480cec6e5680776a8c0497e82
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-04-14 14:01:56 -0300

    create appointment signals

---
+++ appointments/signals.py
@@ -0,0 +1,18 @@
+import logging
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from appointments.models import Appointment
+from utils.send_mail import send_email
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=Appointment)
+def send_mail(sender, instance, **kwargs):
+    sender_mail = 'lucas.dcs.dev@gmail.com'
+    sender_password = '12092003ldc'
+    recipient_email = 'lucas.dcs.dev@gmail.com'
+    subject = 'test'
+    body = 'test'
+
+    send_email(sender_mail, sender_password, recipient_email, subject, body)
