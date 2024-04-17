36bcc75f9fccaf80d1bb23e30367b758a0b54d9a
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-04-17 01:29:00 -0300

    create appointment context path

--- allineduate_back/urls.py
+++ allineduate_back/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('clinic/', include('clinics.urls')),
+    path('appointment/', include('appointments.urls')),
 ]
