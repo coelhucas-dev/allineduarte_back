0ea29bdd1e9e6aa3731e1363ad61e0dce50ed458
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-04-17 01:29:19 -0300

    add plan to the list_display in django_admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -5,6 +5,6 @@ from appointments.models import Appointment
 
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ['clinic', 'time', 'confirmed']
+    list_display = ['clinic', 'plan', 'time', 'confirmed']
     search_fields = ['clinic__name']
     list_editable = ['confirmed']
