80e16a7fd5d0d7a1b04a7194c7614f912d16e8a6
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-05-11 11:10:54 -0300

    fix name from allineduate to allineduarte

--- allineduarte_back/asgi.py
+++ allineduarte_back/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for allineduate_back project.
+ASGI config for allineduarte_back project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "allineduate_back.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "allineduarte_back.settings")
 
 application = get_asgi_application()
--- allineduarte_back/wsgi.py
+++ allineduarte_back/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for allineduate_back project.
+WSGI config for allineduarte_back project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "allineduate_back.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "allineduarte_back.settings")
 
 application = get_wsgi_application()
