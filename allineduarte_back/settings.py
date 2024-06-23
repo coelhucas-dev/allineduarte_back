d8d463982f09bebfae366e0769de36606f651980
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 19:25:29 -0300

    feat: update allowed_hosts to env var

--- allineduarte_back/settings.py
+++ allineduarte_back/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = list(env('ALLOWED_HOSTS'))
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
