a90190821c6559696d1c7d81423dcaa9f114f2b3
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 20:35:29 -0300

    create uwsgi params

--- allineduarte_back/settings.py
+++ allineduarte_back/settings.py
@@ -162,10 +162,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # STATICFILES_DIRS = ( os.path.join(BASE_DIR,'assets/'),)
 
