6db8e8de6d35583b60a8929f5bccc9863995e0b6
Lucas Coelho <118553341+luuh-dcoelho@users.noreply.github.com>
2024-06-23 20:15:03 -0300

    Update settings.py

--- allineduarte_back/settings.py
+++ allineduarte_back/settings.py
@@ -162,8 +162,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/assets/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
+
+STATICFILES_DIRS = ( os.path.join(BASE_DIR,'assets/'),)
 
 
 # Default primary key field type
